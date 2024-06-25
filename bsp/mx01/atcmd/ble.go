6a1185cc211839172033f71b11cab99beb948cf6
renshan1 <1503160585@qq.com>
2024-06-25 21:15:27 +0800

    updata mx01

--- bsp/mx01/atcmd/ble.go
+++ bsp/mx01/atcmd/ble.go
@@ -52,3 +52,176 @@ func NAME(Mx01 device.Device) (string, error) {
 	}
 	return "", fmt.Errorf("invalid result:%v", ATResponse.String())
 }
+
+/*
+*
+* AT+ADV?
+*
+ */
+
+func ADV(Mx01 device.Device) (string, error) {
+	ATResponse, err := Mx01.AT("AT+ADV?\r\n", 1000*time.Millisecond)
+	if err != nil {
+		return "", err
+	}
+	if len(ATResponse.Data) == 1 {
+		return ATResponse.Data[0], nil
+	}
+	return "", fmt.Errorf("invalid result:%v", ATResponse.String())
+}
+
+/*
+*
+* AT+UART?
+*
+ */
+
+func UART(Mx01 device.Device) (string, error) {
+	ATResponse, err := Mx01.AT("AT+UART?\r\n", 1000*time.Millisecond)
+	if err != nil {
+		return "", err
+	}
+	if len(ATResponse.Data) == 1 {
+		return ATResponse.Data[0], nil
+	}
+	return "", fmt.Errorf("invalid result:%v", ATResponse.String())
+}
+
+/*
+*
+* AT+DEV? 查询当前已连接的设备
+*
+ */
+
+func DEV(Mx01 device.Device) (string, error) {
+	ATResponse, err := Mx01.AT("AT+DEV?\r\n", 1000*time.Millisecond)
+	if err != nil {
+		return "", err
+	}
+	if len(ATResponse.Data) == 0 {
+		return "No device connected", nil
+	}
+	if len(ATResponse.Data) == 1 {
+		return ATResponse.Data[0], nil
+	}
+	return "", fmt.Errorf("invalid result:%v", ATResponse.String())
+}
+
+/*
+*
+* AT+AINTVL? 查询广播间隔
+*
+ */
+
+func AINTVL(Mx01 device.Device) (string, error) {
+	ATResponse, err := Mx01.AT("AT+AINTVL?\r\n", 1000*time.Millisecond)
+	if err != nil {
+		return "", err
+	}
+	if len(ATResponse.Data) == 1 {
+		return ATResponse.Data[0], nil
+	}
+	return "", fmt.Errorf("invalid result:%v", ATResponse.String())
+}
+
+/*
+*
+* AT+VER? 读取软件版本
+*
+ */
+
+func VER(Mx01 device.Device) (string, error) {
+	ATResponse, err := Mx01.AT("AT+VER?\r\n", 1000*time.Millisecond)
+	if err != nil {
+		return "", err
+	}
+	if len(ATResponse.Data) == 1 {
+		return ATResponse.Data[0], nil
+	}
+	return "", fmt.Errorf("invalid result:%v", ATResponse.String())
+}
+
+/*
+*
+* AT+TXPOWER? 查询模组的发射功率
+*
+ */
+
+func TXPOWER(Mx01 device.Device) (string, error) {
+	ATResponse, err := Mx01.AT("AT+TXPOWER?\r\n", 1000*time.Millisecond)
+	if err != nil {
+		return "", err
+	}
+	if len(ATResponse.Data) == 1 {
+		return ATResponse.Data[0], nil
+	}
+	return "", fmt.Errorf("invalid result:%v", ATResponse.String())
+}
+
+/*
+*
+* AT+UUIDS? 查询 BLE 主服务通道
+*
+ */
+
+func UUIDS(Mx01 device.Device) (string, error) {
+	ATResponse, err := Mx01.AT("AT+UUIDS?\r\n", 1000*time.Millisecond)
+	if err != nil {
+		return "", err
+	}
+	if len(ATResponse.Data) == 1 {
+		return ATResponse.Data[0], nil
+	}
+	return "", fmt.Errorf("invalid result:%v", ATResponse.String())
+}
+
+/*
+*
+* AT+UUIDN? 查询 BLE 读服务通道
+*
+ */
+
+func UUIDN(Mx01 device.Device) (string, error) {
+	ATResponse, err := Mx01.AT("AT+UUIDN?\r\n", 1000*time.Millisecond)
+	if err != nil {
+		return "", err
+	}
+	if len(ATResponse.Data) == 1 {
+		return ATResponse.Data[0], nil
+	}
+	return "", fmt.Errorf("invalid result:%v", ATResponse.String())
+}
+
+/*
+*
+* AT+UUIDW? 查询 BLE 写服务通道
+*
+ */
+
+func UUIDW(Mx01 device.Device) (string, error) {
+	ATResponse, err := Mx01.AT("AT+UUIDW?\r\n", 1000*time.Millisecond)
+	if err != nil {
+		return "", err
+	}
+	if len(ATResponse.Data) == 1 {
+		return ATResponse.Data[0], nil
+	}
+	return "", fmt.Errorf("invalid result:%v", ATResponse.String())
+}
+
+/*
+*
+* AT+AMDATA? 查询自定义广播数据
+*
+ */
+
+func AMDATA(Mx01 device.Device) (string, error) {
+	ATResponse, err := Mx01.AT("AT+AMDATA?\r\n", 1000*time.Millisecond)
+	if err != nil {
+		return "", err
+	}
+	if len(ATResponse.Data) == 1 {
+		return ATResponse.Data[0], nil
+	}
+	return "", fmt.Errorf("invalid result:%v", ATResponse.String())
+}
